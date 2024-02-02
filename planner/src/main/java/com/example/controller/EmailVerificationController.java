ddbe58bf89fef2f1787d5a672c85accb85fabf69
newb-ver4 <shutdown2423@gmail.com>
2024-02-02 14:09:26 +0900

    test

--- planner/src/main/java/com/example/controller/EmailVerificationController.java
+++ planner/src/main/java/com/example/controller/EmailVerificationController.java
@@ -7,12 +7,15 @@ import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.example.service.EmailService;
 
 
 @RestController
---
+++ planner/src/main/java/com/example/service/PlannerService.java
@@ -0,0 +1,50 @@
+package com.example.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Plan;
+import com.example.repository.PlannerRepository;
+
+@Service
+public class PlannerService {
+
+	@Autowired
+	public PlannerRepository plannerRepository;
+	
+	// 일정 추가
+	public Plan insert(Plan plan) {
+		System.out.println("저장완료");
+		return plannerRepository.save(plan);
+	}
+
+	// 일정 조회
+	public List<Map<String, Object>> selectList(String userEmail) {
+		List<Plan> planList = plannerRepository.findAllByuserEmail(userEmail);
+
+		// Plan 객체를 Map으로 변환하는 로직을 추가하면 됩니다.
+        List<Map<String, Object>> eventList = 
+        		planList.stream().map(plan -> {
+							        		    Map<String, Object> event = new HashMap<>();
+							        		    event.put("title", plan.getTitle());
+							        		    event.put("start", plan.getStartDate());
+							        		    event.put("end", plan.getEndDate());
+							        		    // 필요한 필드들을 추가로 넣어주면 됩니다.
+							        		    return event;
+							        		})
+							        		.collect(Collectors.toList());
+
+        return eventList;
+	}
+
+	/*public Optional<Plan> selectDetail(Long planNo, String userEmail) {
+		return plannerRepository.findByUserEmailAndPlanNo(planNo, userEmail);
+	}*/
+
+}
